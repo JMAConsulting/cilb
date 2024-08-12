2b287a3c0c99ab646389f5b5b3ebe7c4de06d333
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-08-12 14:08:09 -0400

    Changing API calls to not rely on local storage

--- web/modules/custom/cilb_candidate_registration/js/getScopeDefn.js
+++ web/modules/custom/cilb_candidate_registration/js/getScopeDefn.js
@@ -6,7 +6,6 @@ jQuery(document).ready(function ($) {
         'input[name="select_your_exam_category"]:checked'
       ).val();
 
-      
       var checkedRadioId = $(
         "input[name='select_your_exam_category']:checked"
       ).attr("id");
@@ -18,37 +17,7 @@ jQuery(document).ready(function ($) {
       $("#edit-civicrm-1-participant-1-participant-event-id").val(
         selectedValue
       );
-
-      getEventDescription(selectedValue);
     });
-
-    // Get event description
-    function getEventDescription(eventId) {
-      var apiUrl = "/civicrm/ajax/rest?entity=Event&action=get&json=1";
-
-      $.ajax({
-        url: apiUrl,
-        type: "POST",
-        data: {
-          entity: "Event",
-          action: "get",
-          json: 1,
-          id: eventId,
-          select: ["description"],
-        },
-        success: function (data) {
-          if (data.is_error === 0 && data.values && data.values[eventId]) {
-            var description = data.values[eventId].description;
-            localStorage.setItem("temp_markup", description);
-          } else {
-            console.log("Error or no description found in the response");
-          }
-        },
-        error: function (xhr, status, error) {
-          console.log("AJAX error: " + error);
-        },
-      });
-    }
 
     $('input[name="select_your_exam_category"]:checked').trigger("change");
   }
--- web/modules/custom/cilb_candidate_registration/js/loadScopeDefn.js
+++ web/modules/custom/cilb_candidate_registration/js/loadScopeDefn.js
@@ -1,56 +1,71 @@
 jQuery(document).ready(function ($) {
-  if ($("#edit-scope-markup").length) {
-    var storedMarkup = localStorage.getItem("temp_markup");
-    if (storedMarkup) {
-      $("#edit-scope-markup").html(storedMarkup);
-    }
+  // Check if form is in English or Spanish
+  var currentUrl = window.location.href;
+  var lang;
 
-    var examChoice = localStorage.getItem("exam_choice");
-
-    if (examChoice) {
-      $(".fieldset__legend .fieldset__label").append(" - " + examChoice);
-    }
+  // Check if '/es/' is in the URL
+  if (currentUrl.indexOf("/es/") !== -1) {
+    lang = "es_MX";
+  } else {
+    lang = "en_US";
   }
 
-  if ($("#edit-civicrm-1-participant-1-participant-fee-amount").length) {
-    var examChoice = $(
-      "#edit-civicrm-1-participant-1-participant-event-id"
-    ).val();
+  var examChoice = $(
+    "#edit-civicrm-1-participant-1-participant-event-id"
+  ).val();
 
+  if ($("#edit-civicrm-1-participant-1-participant-fee-amount").length) {
     getPriceSet(examChoice);
+  }
+  if ($("#edit-scope-markup").length) {
+    CRM.api4("Event", "get", {
+      select: ["description", "title"],
+      where: [["id", "=", examChoice]],
+      language: lang,
+      checkPermissions: false,
+      limit: 1,
+    }).then(function (events) {
+      console.log(events);
+      $("#edit-scope-markup").html(events[0]["description"]);
+      $(".fieldset__legend .fieldset__label").append(
+        " - " + events[0]["title"]
+      );
+    });
   }
 
   function getPriceSet(eventId) {
     CRM.api4("PriceSetEntity", "get", {
-      select: ["price_field_value.*"],
-      join: [
-        [
-          "PriceFieldValue AS price_field_value",
-          "INNER",
-          ["price_set_id", "=", "price_field_value.price_field_id"],
-        ],
-      ],
+      select: ["price_set_id"],
       where: [
         ["entity_table", "=", "civicrm_event"],
         ["entity_id", "=", eventId],
       ],
+      language: lang,
+      checkPermissions: false,
     }).then(
-      function (priceSetEntities) {
-        // Check if the priceSetEntities array contains data
-        if (priceSetEntities.length > 0) {
-          // Clear any existing checkboxes
-          $("#edit-exam-fee-markup").empty();
+      function (priceSets) {
+        // Extract the price_set_id values into an array
+        var priceSetIds = priceSets.map(function (priceSet) {
+          return priceSet.price_set_id;
+        });
 
-          // Iterate through the priceSetEntities array and create new checkboxes
-          priceSetEntities.forEach(function (item, index) {
-            var label =
-              item["price_field_value.label"] +
-              " - $" +
-              item["price_field_value.amount"];
-            var amount = item["price_field_value.amount"];
+        CRM.api4("PriceFieldValue", "get", {
+          where: [["price_field_id", "IN", priceSetIds]],
+          language: lang,
+          checkPermissions: false,
+        }).then(function (priceFieldValues) {
+          // Check if the priceSetEntities array contains data
+          if (priceFieldValues.length > 0) {
+            // Clear any existing checkboxes
+            $("#edit-exam-fee-markup").empty();
 
-            // Create a new checkbox input and label
-            var checkboxHtml = `
+            // Iterate through the priceSetEntities array and create new checkboxes
+            priceFieldValues.forEach(function (item, index) {
+              var label = item["label"] + " - $" + item["amount"];
+              var amount = item["amount"];
+
+              // Create a new checkbox input and label
+              var checkboxHtml = `
                 <div class="form-type-boolean js-form-item form-item js-form-type-checkbox form-item-exam-fee-front-end-${index} js-form-item-exam-fee-front-end-${index}">
                     <input class="civicrm-enabled form-checkbox form-boolean form-boolean--type-checkbox exam-fee-checkbox" 
                         data-civicrm-field-key="exam_fee_front_end" 
@@ -63,21 +78,22 @@ jQuery(document).ready(function ($) {
                 </div>
                 `;
 
-            // Append the new checkbox to the new container
-            $("#edit-exam-fee-markup").append(checkboxHtml);
-          });
+              // Append the new checkbox to the new container
+              $("#edit-exam-fee-markup").append(checkboxHtml);
+            });
 
-          // Add event listener to checkboxes (only once, after they've all been added)
-          $("#edit-exam-fee-markup").on(
-            "change",
-            ".exam-fee-checkbox",
-            function () {
-              updateFeeAmount();
-            }
-          );
-        } else {
-          console.log("No data found in the response");
-        }
+            // Add event listener to checkboxes (only once, after they've all been added)
+            $("#edit-exam-fee-markup").on(
+              "change",
+              ".exam-fee-checkbox",
+              function () {
+                updateFeeAmount();
+              }
+            );
+          } else {
+            console.log("No data found in the response");
+          }
+        });
       },
       function (failure) {
         // Handle failure
