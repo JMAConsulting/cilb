f087b131e8fcc5a7f6c36e92d148cf9cf1abd91c
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-08-15 13:53:13 -0400

    Updating library name and fixing JS selector

--- web/modules/custom/cilb_reschedule_exam/js/populateLocation.js
+++ web/modules/custom/cilb_reschedule_exam/js/populateLocation.js
@@ -10,34 +10,53 @@ jQuery(document).ready(function ($) {
     lang = "en_US";
   }
 
-  $("#edit-proposed-exam-date").on("change", function () {
-    var selectedValue = $(this).val();
+  $('[data-drupal-selector="edit-proposed-exam-date"]').on(
+    "change",
+    function () {
+      var selectedValue = $(this).val();
 
-    CRM.api4("Event", "get", {
-      select: ["address.*"],
-      join: [
-        [
-          "Address AS address",
-          "LEFT",
-          ["loc_block_id.address_id", "=", "address.id"],
+      console.log("Selected Value:", selectedValue);
+
+      // Get event location
+      CRM.api4("Event", "get", {
+        select: ["address.*"],
+        join: [
+          [
+            "Address AS address",
+            "LEFT",
+            ["loc_block_id.address_id", "=", "address.id"],
+          ],
         ],
-      ],
-      where: [["id", "=", selectedValue]],
-      language: lang,
-    }).then(
-      function (events) {
-        selectedEvent = events[0];
-        $("#edit-proposed-location").val(
-          selectedEvent.address.street_address +
-            ", " +
-            selectedEvent.address.city +
-            ", " +
-            selectedEvent["address.state_province_id:abbr"]
-        );
-      },
-      function (failure) {
-        console.log(failure);
-      }
-    );
-  });
+        where: [["id", "=", selectedValue]],
+        language: lang,
+      }).then(
+        function (events) {
+          if (events.length > 0) {
+            var selectedEvent = events[0];
+            console.log("Selected Event:", selectedEvent);
+
+            // Check if address exists
+            if (selectedEvent.address) {
+              var fullAddress =
+                selectedEvent.address.street_address +
+                ", " +
+                selectedEvent.address.city +
+                ", " +
+                selectedEvent["address.state_province_id:abbr"];
+
+              // Set the full address in the input field
+              $("#edit-proposed-location").val(fullAddress);
+            } else {
+              console.log("Address not found for the selected event.");
+            }
+          } else {
+            console.log("No events found with the selected ID.");
+          }
+        },
+        function (failure) {
+          console.log("API Call Failed:", failure);
+        }
+      );
+    }
+  );
 });
