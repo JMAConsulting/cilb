f1659e5da31a317594c85f6956ea366070db840d
ben <ben@jmaconsulting.biz>
2024-10-02 15:17:55 +0100

    join through exam_registration_part to get registration part and status

--- web/sites/default/files/civicrm/ext/cilb_import/Civi/Api4/Action/Cilb/ImportRegistrations.php
+++ web/sites/default/files/civicrm/ext/cilb_import/Civi/Api4/Action/Cilb/ImportRegistrations.php
@@ -13,11 +13,20 @@ namespace Civi\Api4\Action\Cilb;
 class ImportRegistrations extends ImportBase {
 
   protected function import() {
+    $this->importParts();
+    $this->importBusinessAndFinance();
+  }
+
+  protected function importParts() {
     foreach ($this->getRows("
-        SELECT FK_Account_ID, FK_Category_ID, Confirm_BF_Exam, Category_Name
+        SELECT PK_Exam_Registration_ID, FK_Account_ID, FK_Category_ID, Category_Name, Transaction_Date, Exam_Part_Name_Abbr, Pass
         FROM pti_exam_registrations
         JOIN pti_code_categories
         ON `FK_Category_ID` = `PK_Category_ID`
+        JOIN pti_exam_registration_parts
+        ON `FK_Exam_Registration_ID` = `PK_Exam_Registration_ID`
+        JOIN pti_category_exam_parts
+        ON `FK_Exam_Part_ID` = `PK_Exam_Part_ID`
         WHERE Transaction_Date > '{$this->cutOffDate}'
     ") as $registration) {
 
@@ -30,19 +39,63 @@ class ImportRegistrations extends ImportBase {
         continue;
       }
 
-      $categoryEvents = \Civi\Api4\Event::get(FALSE)
-        ->addWhere('event_type_id:name', '=', $registration['Category_Name']);
+      $event = \Civi\Api4\Event::get(FALSE)
+        ->addSelect('id', 'Exam_Details.Exam_Part')
+        ->addWhere('event_type_id:name', '=', $registration['Category_Name'])
+        ->addWhere('Exam_Details.Exam_Part', '=', $registration['Exam_Part_Name_Abbr'])
+        ->execute()
+        ->first();
 
-      if (!$registration['Confirm_BF_Exam']) {
-        $categoryEvents->addWhere('Exam_Details.Exam_Part', '!=', 'BF');
+      if (!$event) {
+        $debug = json_encode($registration);
+        Civi::log()->warning("No event found for registration ID {$registration['PK_Exam_Registration_ID']}. ({$debug})");
       }
 
-      $categoryEvents = $categoryEvents->execute();
+      \Civi\Api4\Participant::create(FALSE)
+        ->addValue('event_id', $event['id'])
+        ->addValue('contact_id', $contactId)
+        ->addValue('register_date', $registration['Transaction_Date'])
+        ->addValue('status_id:label', $registration['Pass'] ? 'Pass' : 'Fail')
+        ->execute();
+    }
+  }
 
-      foreach ($categoryEvents as $event) {
+  protected function importBusinessAndFinance() {
+    foreach ($this->getRows("
+        SELECT PK_Exam_Registration_ID, FK_Account_ID, FK_Category_ID, Category_Name, Confirm_BF_Exam, BF_Pass, Transaction_Date
+        FROM pti_exam_registrations
+        JOIN pti_code_categories
+        ON `FK_Category_ID` = `PK_Category_ID`
+        WHERE Transaction_Date > '{$this->cutOffDate}'
+        AND Confirm_BF_Exam IS NOT NULL
+    ") as $registration) {
+
+      $contactId = \Civi\Api4\Contact::get(FALSE)
+        ->addWhere('external_identifier', '=', $registration['FK_Account_ID'])
+        ->execute()->first()['id'] ?? NULL;
+
+      if (!$contactId) {
+        \Civi::log()->warning('No contact id found for Account ID: ' . $registration['FK_Account_ID']);
+        continue;
+      }
+
+      $event = \Civi\Api4\Event::get(FALSE)
+        ->addSelect('id')
+        ->addWhere('event_type_id:name', '=', $registration['Category_Name'])
+        ->addWhere('Exam_Details.Exam_Part', '=', 'BF')
+        ->execute()
+        ->first();
+
+      if (!$event) {
+        $debug = json_encode($registration);
+        Civi::log()->warning("No event found for registration ID {$registration['PK_Exam_Registration_ID']}. ({$debug})");
+      }
+
         \Civi\Api4\Participant::create(FALSE)
           ->addValue('event_id', $event['id'])
           ->addValue('contact_id', $contactId)
+          ->addValue('register_date', $registration['Transaction_Date'])
+          ->addValue('status_id:label', $registration['BF_Pass'] ? 'Pass' : 'Fail')
           ->execute();
       }
     }
