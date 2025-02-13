dc28f9f31f0fa1dab1c4c4e1959c4151260869f9
ben <ben@jmaconsulting.biz>
2025-02-13 09:51:15 +0000

    lint fetchLocationDetails function

--- web/modules/custom/cilb_candidate_registration/js/selectedExamInfo.js
+++ web/modules/custom/cilb_candidate_registration/js/selectedExamInfo.js
@@ -11,6 +11,38 @@ jQuery(document).ready(function ($) {
     lang = "en_US";
   }
 
+  function fetchLocationDetail(eventID) {
+    CRM.api4('Event', 'get', {
+      select: ["address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.city", "address.state_province_id:abbr", "address.country_id:label", "address.postal_code"],
+      join: [["Address AS address", "LEFT", ["loc_block_id.address_id", "=", "address.id"]]],
+      where: [["id", "IN", eventID], ["Exam_Details.Exam_Part", "<>", "BF"]],
+    }).then(function(events) {
+      if (events.length > 0) {
+        $('#edit-location-detail').show();
+
+        var supplemental_address_2 = events[0]["address.supplemental_address_2"] == null ? '' : events[0]["address.supplemental_address_2"] + '<br/>',
+            postal_code = events[0]["address.postal_code"] == null ? '' : events[0]["address.postal_code"] + '<br/>',
+            street_address = events[0]["address.street_address"] == null ? '' : events[0]["address.street_address"] + '<br/>',
+            supplemental_address_1 = events[0]["address.supplemental_address_1"] == null ? '' : events[0]["address.supplemental_address_1"] + '<br/>',
+            state_province_id = events[0]["address.state_province_id:abbr"] == null ? '' : events[0]["address.state_province_id:abbr"] + ",",
+            country_id = events[0]["address.country_id:label"]  == null ? '' : events[0]["address.country_id:label"] + "<br/>";
+
+        var address = street_address
+         + supplemental_address_1
+         + supplemental_address_2
+         + postal_code
+         + state_province_id  + country_id + "<br/>";
+
+        $('#location_detail').html(address);
+      }
+      else {
+       $('#edit-location-detail').hide();
+      }
+    }, function(failure) {
+      // handle failure
+    });
+  }
+
   const $contributionAmountField = $("#edit-civicrm-1-contribution-1-contribution-total-amount");
   $contributionAmountField.parent().hide();
 
@@ -28,36 +60,8 @@ jQuery(document).ready(function ($) {
     $('#edit-location-detail').hide();
   }
 
-   $('#edit-exam-preference').on('change', function(e) { fetchLocationDetail($(this).val()); });
-function fetchLocationDetail(eventID) {
-CRM.api4('Event', 'get', {
-  select: ["address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.city", "address.state_province_id:abbr", "address.country_id:label", "address.postal_code"],
-  join: [["Address AS address", "LEFT", ["loc_block_id.address_id", "=", "address.id"]]],
-  where: [["id", "IN", eventID], ["Exam_Details.Exam_Part", "<>", "BF"]],
-  checkPermissions: false // IGNORED: permissions are always enforced from client-side requests
-}).then(function(events) {
-if (events.length > 0) {
- $('#edit-location-detail').show();
-  var supplemental_address_2 = events[0]["address.supplemental_address_2"] == null ? '' : events[0]["address.supplemental_address_2"] + '<br/>',
-postal_code = events[0]["address.postal_code"] == null ? '' : events[0]["address.postal_code"] + '<br/>',
-street_address = events[0]["address.street_address"] == null ? '' : events[0]["address.street_address"] + '<br/>',
-supplemental_address_1 = events[0]["address.supplemental_address_1"] == null ? '' : events[0]["address.supplemental_address_1"] + '<br/>',
-state_province_id = events[0]["address.state_province_id:abbr"] == null ? '' : events[0]["address.state_province_id:abbr"] + ",",
-country_id = events[0]["address.country_id:label"]  == null ? '' : events[0]["address.country_id:label"] + "<br/>";
-  var address = street_address
-   + supplemental_address_1
-   + supplemental_address_2
-   + postal_code
-   + state_province_id  + country_id + "<br/>";
-$('#location_detail').html(address);
-}
-else {
- $('#edit-location-detail').hide();
-}
-}, function(failure) {
-  // handle failure
-});
-}
+  $('#edit-exam-preference').on('change', function(e) { fetchLocationDetail($(this).val()); });
+
 
   if ($scopeMarkup.length) {
     $scopeMarkup.empty();
