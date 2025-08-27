d785067c2dca3b9d76c802d5034720da31d64172
ben <ben@jmaconsulting.biz>
2025-08-27 18:54:38 +0100

    update migration script

--- web/sites/default/files/civicrm/ext/cilb_import/script/migration.sh
+++ web/sites/default/files/civicrm/ext/cilb_import/script/migration.sh
@@ -9,27 +9,30 @@ BACKUP_FOLDER=data
 
 mkdir -p $BACKUP_FOLDER/$MIGRATION_NAME
 
+cv api4 Cilb.importAccounts cutOffDate=$CUT_OFF_DATE
+$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/1-accounts.sql
+
 cv api4 Cilb.importCandidates cutOffDate=$CUT_OFF_DATE
-$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/1-candidates.sql
+$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/2-candidate-data.sql
 
 cv api4 Cilb.importCandidateEntities cutOffDate=$CUT_OFF_DATE
-$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/2-candidate-entities.sql
+$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/3-candidate-entities.sql
 
 cv api4 Cilb.importExams cutOffDate=$CUT_OFF_DATE
-$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/3-exams.sql
+$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/4-exams.sql
 
 for YEAR in 2019 2020 2021 2022 2023 2024 2025; do
 
   cv api4 Cilb.importRegistrations cutOffDate=$CUT_OFF_DATE transactionYear=$YEAR
-  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/4-$YEAR-1-registrations.sql
+  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/5-$YEAR-1-registrations.sql
 
   cv api4 Cilb.importRegistrationsBF cutOffDate=$CUT_OFF_DATE transactionYear=$YEAR
-  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/4-$YEAR-2-registrations-bf.sql
+  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/5-$YEAR-2-registrations-bf.sql
 
   cv api4 Cilb.importActivities cutOffDate=$CUT_OFF_DATE transactionYear=$YEAR
-  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/4-$YEAR-3-activities.sql
+  $DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/5-$YEAR-3-activities.sql
 
 done
 
 cv api4 Cilb.importBlockedUsers cutOffDate=$CUT_OFF_DATE
-$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/5-blocked-users.sql
+$DB_DUMP > $BACKUP_FOLDER/$MIGRATION_NAME/6-blocked-users.sql
