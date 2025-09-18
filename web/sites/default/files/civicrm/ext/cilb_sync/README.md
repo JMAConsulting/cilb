96eb7b30edfff0cf1edd6b61dedddbea62e559cb
Celine Nicolas <celine.nicolas@jmaconsulting.biz>
2025-09-18 16:29:33 -0400

    CU-86dxqmdxt initial commit for extension

---
+++ web/sites/default/files/civicrm/ext/cilb_sync/README.md
@@ -0,0 +1,3 @@
+# CILB Exam Data Sync
+
+Sync exams data such as score and pass/fail result from different files.
---
+++ web/sites/default/files/civicrm/ext/cilb_sync/cilb_sync.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'cilb_sync.civix.php';
+
+use CRM_CILB_Sync_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function cilb_sync_civicrm_config(&$config): void {
+  _cilb_sync_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function cilb_sync_civicrm_install(): void {
+  _cilb_sync_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function cilb_sync_civicrm_enable(): void {
+  _cilb_sync_civix_civicrm_enable();
+}
