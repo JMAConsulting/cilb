6a3b97593920f3b7801096830cba7bc10bd165f4
Seamus Lee <seamuslee001@gmail.com>
2025-09-23 18:27:19 +1000

    Switch to using a wait for element function instead

--- web/sites/default/files/civicrm/ext/cilb_reports/js/find_candidates_search.js
+++ web/sites/default/files/civicrm/ext/cilb_reports/js/find_candidates_search.js
@@ -1,5 +1,25 @@
-(function($, CRM, angular) {
-  angular.element(document).ready(function() {
+(function($, CRM) {
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+  waitForElm('[id=^event-id]').then(function() {
     const searchParams = new URLSearchParams(window.location.search);
     for (const [key, value] of searchParams) {
       if (key == 'event' && !isNaN(parseFloat(value))) {
@@ -16,4 +36,4 @@
       }
     }
   });
-}(CRM.$, CRM, angular));
+}(CRM.$, CRM));
