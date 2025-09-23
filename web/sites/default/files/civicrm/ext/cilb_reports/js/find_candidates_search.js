df596120a0119cedb2a787bd56a080f087141434
Seamus Lee <seamuslee001@gmail.com>
2025-09-23 18:34:57 +1000

    Final Javascript fixes

--- web/sites/default/files/civicrm/ext/cilb_reports/js/find_candidates_search.js
+++ web/sites/default/files/civicrm/ext/cilb_reports/js/find_candidates_search.js
@@ -13,13 +13,13 @@
       });
 
       // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
-      observer.observe(document.body, {
+      observer.observe(document.documentElement, {
         childList: true,
         subtree: true
       });
     });
   }
-  waitForElm('[id=^event-id]').then(function() {
+  waitForElm("[id^='event-id']").then(function() {
     const searchParams = new URLSearchParams(window.location.search);
     for (const [key, value] of searchParams) {
       if (key == 'event' && !isNaN(parseFloat(value))) {
@@ -27,7 +27,7 @@
       }
       if (key == 'status') {
         const separator = $("[id^='status-id']").attr('ng-list');
-        if (value) {
+        if (value == 'true') {
           $("[id^='status-id']").val(CRM.vars.eventSearch.positiveStatus.join(separator)).trigger('change');
         }
         else {
