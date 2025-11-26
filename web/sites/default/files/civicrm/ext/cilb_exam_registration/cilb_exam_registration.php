0871401e69d6d592f1d6a67cf6a5395127983d91
Monish Deb <monish.deb@jmaconsulting.biz>
2025-11-26 07:01:08 -0500

    86dyg26wm: set user language preference when updating the preferred language and exam lang pref in civicrm

--- web/sites/default/files/civicrm/ext/cilb_exam_registration/cilb_exam_registration.php
+++ web/sites/default/files/civicrm/ext/cilb_exam_registration/cilb_exam_registration.php
@@ -3,6 +3,7 @@
 require_once 'cilb_exam_registration.civix.php';
 
 use CRM_CilbExamRegistration_ExtensionUtil as E;
+use Drupal\user\Entity\User;
 
 /**
  * Implements hook_civicrm_config().
@@ -37,7 +38,56 @@ function cilb_exam_registration_civicrm_enable(): void {
   _cilb_exam_registration_civix_civicrm_enable();
 }
 
+function cilb_exam_registration_civicrm_postProcess($formName, $form) {
+  if ($formName == 'CRM_Contact_Form_Inline_CustomData') {
+    $params = $form->getSubmittedValues();
+    $cfID = \Civi\Api4\CustomField::get(FALSE)
+                  ->addWhere('name', '=', 'Exam_Language_Preference')
+                  ->addWhere('custom_group_id:name', '=', 'Registrant_Info')
+                  ->execute()->first()['id'];
+    foreach ($params as $key => $value) {
+      if ($cfID == CRM_Core_BAO_CustomField::getKeyID($key)) {
+        $contactID = $form->getContactID();
+        if ($ufID = CRM_Core_BAO_UFMatch::getUFId($contactID)) {
+          $user = \Drupal::currentUser()->isAuthenticated() ? User::load($ufID) : NULL;
+          $mapper = [1 => 'en', 2 => 'es'];
+          $user?->set('preferred_langcode', ($mapper[$value] ?? 'en'))->save();
+        }
+      }
+    }
+  }
+}
+
+
 function _cilb_exam_registration_external_identifier_set(\Civi\Core\Event\PreEvent $event) {
+  if ($event->action == 'edit' && \Civi\Api4\Utils\CoreUtil::isContact($event->entity)) {
+    $params = $event->params;
+    $contactID = $params['id'] ?? $params['contact_id'] ?? NULL;
+    if ($contactID) {
+      if ($ufID = CRM_Core_BAO_UFMatch::getUFId($contactID)) {
+        $user = \Drupal::currentUser()->isAuthenticated() ? User::load($ufID) : NULL;
+        if (!empty($params['preferred_language'])) {
+          $langcode = strstr($params['preferred_language'], 'es_') ? 'es' : 'en';
+          $user?->set('preferred_langcode', $langcode)->save();
+        }
+        elseif (!empty($params['custom'])) {
+          $cfID = \Civi\Api4\CustomField::get(FALSE)
+                  ->addWhere('name', '=', 'Exam_Language_Preference')
+                  ->addWhere('custom_group_id:name', '=', 'Registrant_Info')
+                  ->execute()->first()['id'];
+          foreach ($params['custom'] as $id => $value) {
+            foreach ($value as $data) {
+              if (!empty($data['custom_field_id'] == $cfID)) {
+                $mapper = [1 => 'en', 2 => 'es'];
+                $user = \Drupal::currentUser()->isAuthenticated() ? User::load($ufID) : NULL;
+                $user?->set('preferred_langcode', $mapper[$data['value']])->save();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
   if ($event->action == 'create' && \Civi\Api4\Utils\CoreUtil::isContact($event->entity)) {
     $params = $event->params;
     if (empty($params['external_identifier'])) {
