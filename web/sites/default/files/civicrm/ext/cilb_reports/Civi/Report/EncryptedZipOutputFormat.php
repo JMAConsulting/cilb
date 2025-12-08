53722f4e874523c1efa105cc73db144a58d14866
Seamus Lee <seamuslee001@gmail.com>
2025-12-08 18:44:31 +1100

    Further work on getting zip output to work

--- web/sites/default/files/civicrm/ext/cilb_reports/Civi/Report/EncryptedZipOutputFormat.php
+++ web/sites/default/files/civicrm/ext/cilb_reports/Civi/Report/EncryptedZipOutputFormat.php
@@ -7,7 +7,7 @@ use CRM_Report_Form;
 class EncryptedZipOutputFormat extends OutputHandlerBase {
 
   public function isOutputHandlerFor(CRM_Report_Form $form): bool {
-    if (\get_class($form) === 'CRM_CilbReports_Form_Report_MTFReport') {
+    if (\get_class($form) === 'CRM_CilbReports_Form_Report_MTFReport' && $form->getOutputMode() === 'zip') {
       return TRUE;
     }
     return FALSE;
@@ -27,11 +27,16 @@ class EncryptedZipOutputFormat extends OutputHandlerBase {
     $rows = $this->getForm()->getTemplate()->getTemplateVars('rows');
     $form = $this->getForm();
     $csv = \CRM_Report_Utils_Report::makeCsv($form, $rows);
-    $temporaryFileName = '';
+    $temporaryFileName = 'monday_wednesday_friday_report_' . date('Ymd') . '.csv';
+    file_put_contents('/tmp/' . $temporaryFileName, $csv);
     $zip = new \ZipArchive();
     if ($zip->open($this->getFileName(), \ZipArchive::CREATE) === TRUE) {
-
+      $zip->setPassword('Test Password');
+      $zip->addFile('/tmp/' . $temporaryFileName, $temporaryFileName);
+      $zip->setEncryptionName($temporaryFileName, \ZipArchive::EM_AES_256);
+      $zip->close();
     }
+    return $zip;
   }
 
   public function getMimeType(): string {
