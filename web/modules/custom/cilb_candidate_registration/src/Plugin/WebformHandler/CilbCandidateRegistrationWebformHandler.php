9afcebfef93e10ee6f97c68fb37b502bef25c72f
Celine Nicolas <celine.nicolas@jmaconsulting.biz>
2026-01-30 01:06:48 -0500

    Added support for recording exam category

--- web/modules/custom/cilb_candidate_registration/src/Plugin/WebformHandler/CilbCandidateRegistrationWebformHandler.php
+++ web/modules/custom/cilb_candidate_registration/src/Plugin/WebformHandler/CilbCandidateRegistrationWebformHandler.php
@@ -566,6 +566,7 @@ class CilbCandidateRegistrationWebformHandler extends WebformHandlerBase {
           ->addValue('Candidate_Result.Exam_Language_Preference', $webform_submission_data['civicrm_1_contact_1_cg1_custom_3'] ?? NULL)
           ->addValue('Candidate_Result.ADA_Accommodations_Needed', $webform_submission_data['civicrm_1_contact_1_cg1_custom_4'] ?? FALSE)
           ->addValue('Candidate_Result.Registration_Expiry_Date', date('Y-m-d H:i:s', strtotime('+5 years')))
+          ->addValue('Candidate_Result.Candidate_Exam_Category', [$webform_submission_data['select_category_id']])
           ->execute()
           ->first()['id'];
 
