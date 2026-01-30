434811c79af8ae58a900dbffb9cfe8159468f0c0
Celine Nicolas <celine.nicolas@jmaconsulting.biz>
2026-01-30 00:36:54 -0500

    Added B7F exam category field

--- web/sites/default/files/civicrm/ext/cilb_exam_registration/managed/cilb_exam_registration_custom_field.mgd.php
+++ web/sites/default/files/civicrm/ext/cilb_exam_registration/managed/cilb_exam_registration_custom_field.mgd.php
@@ -28,4 +28,29 @@ return [
       ],
     ],
   ],
+  [
+    'name' => 'CustomField_Exam_Category',
+    'entity' => 'CustomField',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'custom_group_id.name' => 'Candidate_Result',
+        'name' => 'Candidate_Exam_Category',
+        'label' => E::ts('B&F Exam Category'),
+        'html_type' => 'Select',
+        'is_searchable' => TRUE,
+        'text_length' => 255,
+        'note_columns' => 60,
+        'note_rows' => 4,
+        'option_group_id.name' => 'event_type',
+        'serialize' => 1,
+      ],
+      'match' => [
+        'name',
+        'custom_group_id',
+      ],
+    ],
+  ],
 ];
